8b74a4f0800a64c2be7476a0814d36fe420f1e4c
Ubuntu <ubuntu@machine978e80d4-ab77-436a-87f0-b7024950ac28.in2p3.fr>
2019-12-02 14:38:54 +0000

    cleaner config

--- config.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-html_version="NCBI" #can also be "MAIN"
-lang="en" #can also be "fr"
-server="" ##without http:// ; can be a named server as well (mydomain.org)
-
-
-if [ $server=="" ] 
-
--- configure_html.sh
+++ configure_html.sh
@@ -1,6 +1,46 @@
 #! /bin/sh
 
-##COPY CORRECT HTTP TO /VAR/WWW/HTML AND UPDATE HTML FILE
+usage="$(basename "$0") [-h] [-s server_address] -- update apache2 and Lifemap html scripts with specific server asdress. 
+
+where:
+    -h  show this help text
+    -s  server to use. (default: ip adress of the machine on the web)"
+
+server="default.default"
+while getopts ':hs:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    s) server=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+
+#html_version="NCBI" #can also be "MAIN"
+#lang="en" #can also be "fr"
+#server="default.default" ##IP address of domain. without http. If not specified, IP adress is retrieved from with command ifconfig.me 
+
+if [ $server=="default.default" ]; then
+        echo "Getting IP adress of machine..."
+	server=$(curl ifconfig.me)
+fi
+echo "Exporting server address to /etc/lifemap_envir"
+sudo echo "export SERVER_ADDRESS="$server > /etc/lifemap_envir
+echo "Restarting apache"
+sudo service apache2 restart
+
+##COPY CORRECT HTTP TO /VAR/WWW/HTML AND UPDATE HTML FILE with server name
 if [ $html_version=="NCBI" ]; then
         sudo cp -r html/HTTP-NCBI/* /var/www/html
         sudo sed -i s/"lifemap.univ-lyon1.fr"/$server/g /var/www/html/index.html
