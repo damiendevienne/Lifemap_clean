ee85a5cba5951653d2ca9fcb12e111a65362cf74
Ubuntu <ubuntu@machine978e80d4-ab77-436a-87f0-b7024950ac28.in2p3.fr>
2019-12-06 12:31:31 +0000

    load renderdlist service at the end of tree creation

--- make_tree.py
+++ make_tree.py
@@ -8,6 +8,13 @@ parser.add_argument('--lang', nargs='?', help='Language chosen. FR for french, E
 parser.add_argument('--simplify', nargs='?', help='Should the tree be simplified by removing environmental and unindentified species?', choices=['True','False'])
 args = parser.parse_args()
 
-
+#write the options chosen to a parameters file for later updates with same options
+f= open("/usr/local/lifemap/TREEOPTIONS","w")
+f.write("#!/bin/sh\nlang=\"%s\"\nsimplify=\"%s\"\n"%(args.lang, args.simplify))
+f.close()
 os.system("(cd /usr/local/lifemap/ ; sudo ./Main.py --lang %s --simplify %s)" % (args.lang,args.simplify))
+
+##start render_list service for rendering in adva,ce many tiles
+os.system("sudo systemctl start renderdlist")
+
 
