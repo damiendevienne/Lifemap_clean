d7bde6ab767fa15dbce2a2cae83839344544be5b
Votre Nom <Vous@exemple.com>
2023-10-17 11:34:04 +0200

    add nolabels and onlylabels tile rendering

--- tree/preRenderTiles.sh
+++ tree/preRenderTiles.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 /opt/mod_tile/render_list -n 7 < /usr/local/lifemap/XYZcoordinates >> /usr/local/lifemap/tilerenderer.log
+/opt/mod_tile/render_list -m onlylabels -n 7 < /usr/local/lifemap/XYZcoordinates >> /usr/local/lifemap/tilerenderer.log
+/opt/mod_tile/render_list -m nolabels -n 7 < /usr/local/lifemap/XYZcoordinates >> /usr/local/lifemap/tilerenderer.log
+
